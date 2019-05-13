f86a2bdda85f7c3e7350ff367f8f1e6140aaee79
Igor Baiborodine <igor-baiborodine@yandex.com>
2019-05-13 19:33:20 +0000

    Initial commit

---
+++ 7/jdk8-alpine/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+printenv
+
+echo "
+cmd: $0
+args: $@
+Docker user ID:$(id -u)"
--- update.sh
+++ update.sh
@@ -1,2 +1,53 @@
 #!/bin/bash
-set -eo pipefail
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( "$@" )
+if [[ ${#versions[@]} -eq 0 ]]; then
+	versions=( */ )
+fi
+versions=( "${versions[@]%/}" )
+
+travisEnv=
+for version in "${versions[@]}"; do
+    majorVersion="${version%%.*}"
+	releaseVersion="${version%%/*}"
+    echo "majorVersion:$majorVersion, releaseVersion:$releaseVersion"
+
+    variant="$(basename "$version")"
+    echo "variant:$variant"
+
+    javaVariant="${variant%%-*}"
+    echo "javaVariant:$javaVariant"
+
+    if [[ "$javaVariant" != jdk* ]]; then
+        echo >&2 "not sure what to do with $version/$javaVariant re: baseImage; skipping"
+        continue
+    fi
+
+    subVariant="${variant#$javaVariant-}"
+    echo "subVariant:$subVariant"
+
+    case "$subVariant" in
+        alpine|slim)
+            baseImage="openjdk:${javaVariant:3}-${javaVariant:0:3}${subVariant:+-$subVariant}" # ":8-jdk-alpine", ":11-jdk-slim"
+            echo "baseImage:$baseImage"
+            ;;
+        *)
+            echo >&2 "not sure what to do with $version/$subVariant re: baseImage; skipping"
+            continue
+            ;;
+    esac
+
+    sed -r \
+        -e 's/^(ENV APP_VERSION) .*/\1 '"$releaseVersion"'/' \
+        -e 's/^(FROM) .*/\1 '"$baseImage"'/' \
+        "Dockerfile${subVariant:+-$subVariant}.template" \
+        > "$majorVersion/$variant/Dockerfile"
+
+    cp -a docker-entrypoint.sh "$majorVersion/$variant/"
+
+    travisEnv='\n  - '"VERSION=$majorVersion VARIANT=$variant$travisEnv"
+done
+
+travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+echo "$travis" > .travis.yml
