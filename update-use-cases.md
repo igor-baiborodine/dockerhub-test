f446f5b033d53fc0f1efc6bdcf12093b68aea6d0
Igor Baiborodine <igor-baiborodine@yandex.com>
2019-05-31 19:08:46 +0000

    Add supported tag[7.1.4-ga5/jdk8-alpine]

--- update-use-cases.md
+++ update-use-cases.md
@@ -5,45 +5,37 @@
 - Max number of supported releases for each major version: 1
 - Available variants for each supported release: alpine, slim
 - Available LTS JDK versions for each variant: 8 (alpine/slim), 11 (slim)
-- Max number of supported tags that can be added at a time to supported-tags.conf: 1
+- Max number of supported tags that can be added at a time to supported-tags: 1
 
 ### 1. Add first release with one variant/JDK version
 *Expected*:
 - release/variant directory created
 - release/variant directory contains Dockerfile and docker-entrypoint.sh files
 - placeholders replaced with corresponding values in the newly created Dockerfile
-- corresponding VERSION VARIANT added to .travis.yml/env
+- corresponding VERSION VARIANT pair added to .travis.yml/env and supported-tags
 - changes committed and pushed to remote
 - Travis CI job triggered 
-- new image built, tagged with VERSION-VARIANT and Git commit hash and pushed to Docker HUB
+- new image built with VERSION-VARIANT tag and pushed to Docker HUB
 
 ### 2. Add another variant/JDK version for an existing release
 *Expected*:
 - release/variant directory created
 - release/variant directory contains Dockerfile and docker-entrypoint.sh files
 - placeholders replaced with corresponding values in the newly created Dockerfile
-- corresponding VERSION VARIANT added to .travis.yml/env
-- previous VERSION VARIANT removed from .travis.yml/env
+- corresponding VERSION VARIANT pair added to .travis.yml/env and supported-tags
+- previous VERSION VARIANT pair removed from .travis.yml/env
 - changes committed and pushed to remote
 - Travis CI job triggered 
-- new image built, tagged with VERSION-VARIANT and Git commit hash and pushed to Docker HUB
+- new image built with VERSION-VARIANT tag and pushed to Docker HUB
 
-### 3. Remove variant/JDK version for an existing release
-*Expected*:
-- release/variant directory removed
-- corresponding VERSION VARIANT removed from .travis.yml/env
-- changes committed and pushed to remote
-- Travis CI job triggered 
-- no new image pushed to Docker HUB
-
-### 4. Remove all variant for an existing release, add a new release with one variant/JDK version
+### 3. Remove all variants for an existing release, add a new release with one variant/JDK version
 *Expected*:
 - old release directory removed
 - new release/variant directory created
 - new release/variant directory contains Dockerfile and docker-entrypoint.sh files
 - placeholders replaced with corresponding new release values in the newly created Dockerfile
-- corresponding VERSION VARIANT added to .travis.yml/env
-- previous VERSION VARIANT removed from .travis.yml/env
+- all previous VERSION VARIANT pairs removed from .travis.yml/env and supported-tags
+- corresponding VERSION VARIANT pair added to .travis.yml/env and supported-tags
 - changes committed and pushed to remote
 - Travis CI job triggered 
-- new image built, tagged with VERSION-VARIANT and Git commit hash and pushed to Docker HUB
+- new image built with VERSION-VARIANT tag and pushed to Docker HUB
--- update.sh
+++ update.sh
@@ -1,128 +1,73 @@
 #!/usr/bin/env bash
 
-new_supported_tag=
+main() {
+  local supported_tag="$1"
+  echo "supported_tag: $supported_tag"
 
-add_dockerfile() {
-  echo "add_dockerfile(): begin"
-  local versions="$(cat ./supported-tags.conf)"
-  echo "Versions: ${versions}"
-
-  for version in ${versions}; do
-    local release_version="${version%%/*}"
-    local variant="$(basename "$version")"
-    local java_variant="${variant%%-*}"
-
-    if [[ "$java_variant" != jdk* ]]; then
-      echo "Not sure what to do with $version/$java_variant re: base_image; skipping"
-      continue
-    fi
-
-    local sub_variant="${variant#$java_variant-}"
-
-    echo "Version {
-      release_version: $release_version
-      variant: $variant
-      java_variant: $java_variant
-      sub_variant: $sub_variant
-    }"
-
-    if [[ -d "$release_version/$variant" ]]; then
-      echo "Skipping for for $release_version/$variant"
-    else
-      echo "Adding Dockerfile for $release_version/$variant"
-      mkdir -p "$release_version/$variant"
-
-      local base_image=
-      case "$sub_variant" in
-        alpine|slim)
-          base_image="openjdk:${java_variant:3}-${java_variant:0:3}${sub_variant:+-$sub_variant}" # ":8-jdk-alpine", ":11-jdk-slim"
-          echo "base_image:$base_image"
-          ;;
-        *)
-          echo "Not sure what to do with $version/$sub_variant re: base_image; skipping"
-          continue
-          ;;
-      esac
-
-      sed -r \
-          -e 's/^(ENV APP_VERSION) .*/\1 '"$release_version"'/' \
-          -e 's/^(FROM) .*/\1 '"$base_image"'/' \
-          "Dockerfile${sub_variant:+-$sub_variant}.template" \
-          > "$release_version/$variant/Dockerfile"
-
-      cp -a docker-entrypoint.sh "$release_version/$variant/"
-
-      local travis_env='\n  - '"VERSION=$release_version VARIANT=$variant"
-      echo "travis_env: $travis_env"
-
-      local travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travis_env"'" } { printf "%s%s", $0, RS }' .travis.yml)"
-      echo "travis: $travis"
-      echo "Modifying .travis.yml"
-      awk -v content="$travis" 'BEGIN { printf "%s", content }' > .travis.yml
-
-      new_supported_tag="$release_version/$variant"
-    fi
-  done
-
-  echo "add_dockerfile(): end"
-}
-
-remove_dockerfile() {
-  echo "remove_dockerfile(): begin"
-  local supported_tags="$(cat ./supported-tags.conf)"
-
-  for release_path in $(ls -d */); do
-    if [[ ${supported_tags} = *"$release_path"* ]]; then
-      echo "Found in supported tags: release[${release_path::-1}]"
-      cd "$release_path"
-
-      for variant_path in $(ls -d */); do
-        local tag="$release_path${variant_path::-1}"
-
-        if [[ ${supported_tags} = *"$tag"* ]]; then
-          echo "Found in supported tags: variant[$tag]"
-        else
-          echo "Removing directory: variant[$tag]"
-          rm -rf "$variant_path"
-        fi
-      done
-
-      cd ..
-    else
-      echo "Removing directory: release[$release_path]"
-      rm -rf "$release_path"
-    fi
-  done
-  echo "Remove_dockerfile(): end"
-}
-
-commit_push() {
-  echo "commit_push(): begin"
-  echo "new_supported_tag: $new_supported_tag"
-
-  if [[ -z "$new_supported_tag" ]]; then
-    echo "Skipping for for $release_version/$variant"
-  else
-    git add .
-    git commit -m "Add new supported tag: $new_supported_tag"
-    git push
+  if [[ -d "$supported_tag" ]]; then
+    echo "Already supported tag: $supported_tag"
+    exit 1
   fi
 
-  echo "commit_push(): end"
-}
+  local version="$supported_tag"
+  local release_version="${version%%/*}"
+  local variant="$(basename "$version")"
+  local java_variant="${variant%%-*}"
 
-main() {
-  set -e
-  
-  # dump to console
-  exec 2>&1
+  if [[ "$java_variant" != jdk* ]]; then
+    echo "Not supported Java variant: $java_variant"
+    exit 1
+  fi
 
-  # be quite
-  # exec 1> /dev/null 2>&1
+  local sub_variant="${variant#$java_variant-}"
 
-  add_dockerfile
-  remove_dockerfile
-  commit_push
+  if [[ ! (${sub_variant} == alpine || ${sub_variant} == slim) ]] ; then
+    echo "Not supported sub-variant: $sub_variant"
+    exit 1
+  fi
+
+  echo "Version {
+    release_version: $release_version
+    variant: $variant
+    java_variant: $java_variant
+    sub_variant: $sub_variant
+  }"
+
+  echo "Adding Dockerfile for $release_version/$variant"
+  mkdir -p "$release_version/$variant"
+
+  local base_image="openjdk:${java_variant:3}-${java_variant:0:3}${sub_variant:+-$sub_variant}" # ":8-jdk-alpine", ":11-jdk-slim"
+  echo "base_image:$base_image"
+
+  sed -r \
+      -e 's/^(ENV APP_VERSION) .*/\1 '"$release_version"'/' \
+      -e 's/^(FROM) .*/\1 '"$base_image"'/' \
+      "Dockerfile${sub_variant:+-$sub_variant}.template" \
+      > "$release_version/$variant/Dockerfile"
+
+  cp -a docker-entrypoint.sh "$release_version/$variant/"
+
+  local travis_env='\n  - '"VERSION=$release_version VARIANT=$variant"
+  echo "travis_env: $travis_env"
+
+  local travis="$(awk -v 'RS=\n\n' '$1 == "env:" { $0 = "env:'"$travis_env"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+  echo "Modifying .travis.yml"
+  echo "$travis" > .travis.yml
+
+  if grep -q "$release_version" ./supported-tags; then
+    echo "Found in supported-tags: release[$release_version]"
+    echo "$supported_tag" >> ./supported-tags
+  else
+    echo "Not found in supported-tags: release[$release_version]"
+    echo "$supported_tag" > ./supported-tags
+    rm -rf "$release_version"
+  fi
+
+  git add .
+  git commit -m "Add supported tag[$supported_tag]"
+  git push
+
+  echo "add_dockerfile(): end"
 }
 
 main "$@"
