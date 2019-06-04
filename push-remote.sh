ee8d49ad5e113147213ec7b3caf121aeb73645b5
Igor Baiborodine <igor-baiborodine@yandex.com>
2019-06-04 16:49:19 +0000

    Add supported tag [7.5.2-ga9/jdk11-slim]

--- push-remote.sh
+++ push-remote.sh
@@ -2,11 +2,12 @@
 
 main() {
   local github_repo="@1"
+  echo "github_repo: $github_repo"
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
-#  git add .
-#  git commit -m "Update README.md [skip travis]"
-#  git push "https://${TRAVIS_GITHUB_TOKEN}@${github_repo}" HEAD:master > /dev/null 2>&1
+  git add .
+  git commit -m "Update README.md [skip travis]"
+  git push "https://${TRAVIS_GITHUB_TOKEN}@${github_repo}" HEAD:master > /dev/null 2>&1
 }
 
 main "$@"
