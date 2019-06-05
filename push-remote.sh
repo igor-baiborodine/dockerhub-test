d11c3b3ec06d640ac5d288e4336125b18526e1c9
Igor Baiborodine <igor-baiborodine@yandex.com>
2019-06-05 13:11:35 +0000

    Add supported tag [7.6.0-ga9/jdk8-alpine]

--- push-remote.sh
+++ push-remote.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
 main() {
-  local github_repo="@1"
-  echo "github_repo: $github_repo"
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
-  git add .
-  git commit -m "Update README.md [skip travis]"
-  git push "https://${TRAVIS_GITHUB_TOKEN}@${github_repo}" HEAD:master > /dev/null 2>&1
+
+  mkdir ~/temp
+  git clone "https://github.com/igor-baiborodine/$REPO.git" ~/temp
+  cp "~/$REPO/README.md" "~/$REPO/supported-tags" "~/temp/$REPO"
+
+  cd "~/temp/$REPO"
+  git add README.md supported-tags
+  git status
+  git commit -m "$TRAVIS_COMMIT_MESSAGE [skip travis]"
+  git push "https://${TRAVIS_GITHUB_TOKEN}@${REPO}" master > /dev/null 2>&1
 }
 
 main "$@"
