04fa80e1bee1ff38936888023d76cc5290e2d9aa
Igor Baiborodine <igor-baiborodine@yandex.com>
2019-07-02 19:02:23 +0000

    Add supported tag [7.7.11-ga11/jdk8-slim]

--- push-remote.sh
+++ push-remote.sh
@@ -4,15 +4,15 @@ main() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 
-  git clone "https://github.com/igor-baiborodine/$REPO.git" "~/$REPO"
-  cp README.md supported-tags "~/$REPO"
+  git clone "https://github.com/$TRAVIS_REPO_SLUG.git" "~/build/$TRAVIS_REPO_SLUG"
+  cp README.md supported-tags "~/build/$TRAVIS_REPO_SLUG"
 
-  cd "~/$REPO"
+  cd "~/build/$TRAVIS_REPO_SLUG"
   git add README.md supported-tags
 
   git status
   git commit -m "$TRAVIS_COMMIT_MESSAGE [skip travis]"
-  git push "https://${TRAVIS_GITHUB_TOKEN}@github.com/igor-baiborodine/${REPO}" master  > /dev/null 2>&1
+  git push "https://$TRAVIS_GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG" master  > /dev/null 2>&1
 }
 
 main "$@"
